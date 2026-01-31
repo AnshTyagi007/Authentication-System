a00c4d0cde06caa627d084ebe998c5d9d692e173
AnshTyagi007 <anshtyagi1729@gmail.com>
2026-01-31 13:42:52 +0530

    Add MongoDB connection, logging, and global error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const morgan = require("morgan");
+const notFound = require("./middlewares/notFound.middleware");
+const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
+
 
 // Global Middlewares
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Health check route
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "OK" });
 });
+
+// 404 handler
+app.use(notFound);
+
+// Global error handler;
+app.use(errorHandler);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+// Connect DB before starting server
+connectDB();
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
